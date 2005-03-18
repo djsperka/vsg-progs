996a5cb49026818288a7e8944d9ef370fe68975e
dan <djsperka@ucdavis.edu>
2005-03-18 19:42:55 +0000

    Test CED-VSG patch cables connectivity

---
+++ src/CableTest/CableTest.cpp
@@ -0,0 +1,63 @@
+#define __GNU_LIBRARY__
+#include "getopt.h"
+#undef __GNU_LIBRARY__
+
+#include "VSGEX2.H"
+#include "Alertlib.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "dalert.lib")
+#else
+#pragma comment(lib, "alert.lib")
+#endif
+
+using namespace std;
+using namespace alert;
+
+
+
+void DisplayState(long DigState)
+{
+	cout <<"Bit 0: "<<(DigState&vsgDIG0)/vsgDIG0<<endl;
+	cout <<"Bit 1: "<<(DigState&vsgDIG1)/vsgDIG1<<endl;
+	cout <<"Bit 2: "<<(DigState&vsgDIG2)/vsgDIG2<<endl;
+	cout <<"Bit 3: "<<(DigState&vsgDIG3)/vsgDIG3<<endl;
+	cout <<"Bit 4: "<<(DigState&vsgDIG4)/vsgDIG4<<endl;
+	cout <<"Bit 5: "<<(DigState&vsgDIG5)/vsgDIG5<<endl;
+	cout <<"Bit 6: "<<(DigState&vsgDIG6)/vsgDIG6<<endl;
+	cout <<"Bit 7: "<<(DigState&vsgDIG7)/vsgDIG7<<endl;
+}
+
+int main(int argc, char *argv[])
+{
+	int nRetCode = 0;
+
+
+	vsgInit("");
+	Sleep(3000);
+	long StimulusState,PreviousState;
+	StimulusState=vsgIOReadDigitalIn() & 0xff;
+	PreviousState=StimulusState;
+	DisplayState(StimulusState);
+
+	vsgObjCreate();
+	vsgObjSetPixelLevels(1, 2);
+
+
+	while(TRUE)
+	{
+		StimulusState=vsgIOReadDigitalIn() & 0xff;
+		if (StimulusState!=PreviousState)
+		{
+			cout << StimulusState << endl;
+			DisplayState(StimulusState);
+			PreviousState=StimulusState;
+			vsgObjSetTriggers(vsgTRIG_ONPRESENT + vsgTRIG_OUTPUTMARKER, StimulusState, 0);
+			vsgPresent();
+		}
+	}
+
+	return nRetCode;
+}
+
+
