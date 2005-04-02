2318c8b710a96d67fa647e5d0bbde04060e7be9a
dan <djsperka@ucdavis.edu>
2005-04-02 00:20:49 +0000

    Added CoolerMouse

---
+++ AlertRig/src/CoolerMouse/resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by CoolerMouse.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_COOLERMOUSE_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_APPLY                       1000
+#define IDC_X                           1001
+#define IDC_Y                           1002
+#define IDC_W                           1003
+#define IDC_H                           1004
+#define IDC_CONTRAST                    1005
+#define IDC_SPATIAL                     1006
+#define IDC_TEMPORAL                    1007
+#define IDC_ORIENTATION                 1008
+#define IDC_COLORVECTOR                 1009
+#define IDC_SINEWAVE                    1010
+#define IDC_SQUAREWAVE                  1011
+#define IDC_CIRCLE                      1012
+#define IDC_SQUARE                      1013
+#define IDC_FIXATIONX                   1014
+#define IDC_FIXATIONY                   1015
+#define IDC_FIXATIONDIAMETER            1016
+#define IDC_FIXATIONCOLOR               1017
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
