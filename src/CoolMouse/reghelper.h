64bc55b9c52920f50849e5f94a42c401d646553c
dan <djsperka@ucdavis.edu>
2005-11-23 02:08:33 +0000

    *** empty log message ***

---
+++ src/CoolMouse/reghelper.h
@@ -0,0 +1,10 @@
+#include <string>
+#include "alertlib.h"
+
+bool GetRegConfiguration();
+bool GetRegScreenDistance(int& dist);
+bool GetRegFixpt(double& fixX, double& fixY, double &fixD, COLOR_TYPE& c);
+bool GetRegStimulus(alert::ARApertureGratingSpec &stim);
+bool SaveRegStimulus(std::string s);
+
+
