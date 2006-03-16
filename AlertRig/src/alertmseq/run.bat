54bf0525def4ed397389181102c168149027a3c7
dan <djsperka@ucdavis.edu>
2006-03-16 00:04:51 +0000

    *** empty log message ***

--- AlertRig/src/alertmseq/run.bat
+++ AlertRig/src/alertmseq/run.bat
@@ -1,1 +1,1 @@
-C:\work\AlertRig\bin\dalertmseq -m c:\work\acuterig\msequenceterms -a -p 0,0 -d 4 -l 100 -t 4 -D 555 -F 20,20,5,red
+C:\work\AlertRig\bin\dalertmseq -m c:\work\acuterig\msequenceterms -a -p 0,0 -d 4 -l 100 -t 4 -D 555 -f 20,20,5,red
