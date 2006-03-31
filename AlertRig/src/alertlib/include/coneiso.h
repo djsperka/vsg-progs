f721c70be0733a3f0fc393e37b023a93db9f9fd6
dan <djsperka@ucdavis.edu>
2006-03-31 00:54:35 +0000

    Initial add

---
+++ AlertRig/src/alertlib/include/coneiso.h
@@ -0,0 +1,15 @@
+#if !defined(_CONEISO_H_)
+#define _CONEISO_H_
+
+#include "VSGV8.H"
+
+
+bool coneiso_l(VSGTRIVAL& from, VSGTRIVAL& to);
+bool coneiso_m(VSGTRIVAL& from, VSGTRIVAL& to);
+bool coneiso_s(VSGTRIVAL& from, VSGTRIVAL& to);
+
+
+
+
+
+#endif
