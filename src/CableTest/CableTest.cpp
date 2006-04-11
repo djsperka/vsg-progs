075034f6cb4504b58e79fda7965fd630b1d07937
dan <djsperka@ucdavis.edu>
2006-04-11 01:30:03 +0000

    Notes on the mapping between vsg and 1401 digital lines.

--- src/CableTest/CableTest.cpp
+++ src/CableTest/CableTest.cpp
@@ -48,6 +48,18 @@ int main(int argc, char *argv[])
 
 	while(TRUE)
 	{
+		// Read digital input bits. Our cabling aligns the 1401's DIGOUT and VSG's digital input
+		// bits in the following way. 
+		// A spike2 sequence [00000001] is seen (vsgIOReadDigitalIn) here as 0x1. 
+		// We turn around and set the output triggers with that and send it back to spike2. 
+		// Spike2 sees that on its digital input -- its event ports -- at event port 1. 
+		// In other words, if spike2 sends 0x1 out its DIGOUT it is seen by the VSG as 0x1. 
+		// But if VSG sends back trigger bits set 0x1, spike2 sees it on event port 1. 
+		// The VSG reserves its own digital output bit for its frame trigger pulses.
+		// If you call vsgObjSetTriggers with vsgTRIG_ONPRESENT, then you get a pulse when the frame
+		// is presented, and that pulse is seen at the 1401 at event port 0. If you call vsgObjSetTriggers
+		// with vsgTRIG_OUTPUTMARKER, then the 8 bits you can set are received by the 1401 on event ports 1-8
+		// (actually event port 8 doesn't exist). 
 		StimulusState=vsgIOReadDigitalIn() & 0xff;
 		if (StimulusState!=PreviousState)
 		{
