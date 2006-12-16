c7c4bd52b67e836e2c0bc183ea54898a29c28965
dan <djsperka@ucdavis.edu>
2006-12-16 00:28:22 +0000

    test bat script

---
+++ AlertRig/src/StarStim/starstim.bat
@@ -0,0 +1,3 @@
+rem c:\work\alertrig\bin\starstim -a -b gray -f 0,0,1,r -t 5,0,1,r -n 5 -d 555
+rem c:\work\alertrig\bin\starstim -b gray -f 0,0,1,r -t 5,0,1,r -n 5 -d 555
+c:\work\alertrig\bin\starstim -b gray -f 0,0,1,r -t 4,4,1,r -n 7 -d 555
