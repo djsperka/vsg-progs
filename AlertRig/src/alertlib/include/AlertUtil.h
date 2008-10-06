2105145e52c41f30fc8ebb4046abfd4a516e7f6c
devel <djsperka@ucdavis.edu>
2008-10-06 21:15:04 +0000

    Changes for custom colors in background and stim/distractor.

--- AlertRig/src/alertlib/include/AlertUtil.h
+++ AlertRig/src/alertlib/include/AlertUtil.h
@@ -6,6 +6,7 @@ int arutil_color_to_overlay_palette(ARFixationPointSpec& fp, PIXEL_LEVEL level);
 int arutil_color_to_overlay_palette(COLOR_TYPE c, PIXEL_LEVEL level);
 int arutil_color_to_palette(COLOR_TYPE c, PIXEL_LEVEL level);
 int	arutil_draw_overlay(ARFixationPointSpec& fp, PIXEL_LEVEL level, int overlayPage);
+int	arutil_draw_video(ARFixationPointSpec& fp, PIXEL_LEVEL level, int videoPage);
 int arutil_draw_grating_fullscreen(ARGratingSpec& gr, int videoPage);
 int arutil_draw_grating(ARGratingSpec& gr, int videoPage);
 int arutil_draw_aperture(ARGratingSpec& gr, int overlayPage);
