55f99264efc87d7a3af5521f11dd0f4c312dfe1a
devel <djsperka@ucdavis.edu>
2009-01-07 00:54:40 +0000

    Initial add.

---
+++ AlertRig/src/affflash/run.bat
@@ -0,0 +1,2 @@
+..\..\bin\affflash -v -r 4 -l 3 -B 2 -t 4 -c 100 -d 1100 -b gray -m c:\work\acuterig\msequenceterms
+rem ..\..\bin\affflash -r 4 -l 3 -B 2 -t 4 -c 70 -d 1100 -b gray -V (0,0,0)/(1,1,1) -g 1,1,2,2,78,1,0,90,b,s,e -m c:\work\acuterig\msequenceterms
