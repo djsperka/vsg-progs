adc2f88416eb4d7bfee6b22c9aa2616f3313e287
devel <djsperka@ucdavis.edu>
2009-01-09 01:20:23 +0000

    Finally gave up on this. See AcuteFFFlash.

--- AlertRig/src/affflash/run.bat
+++ AlertRig/src/affflash/run.bat
@@ -1,2 +1,2 @@
-rem ..\..\bin\affflash -v -r 2 -l 6 -B 6 -t 4 -c 50 -d 1100 -b gray -m c:\work\acuterig\msequenceterms
-..\..\bin\affflash -r 2 -l 3 -B 3 -t 4 -c 70 -d 1100 -b gray -g 1,1,2,2,78,1,0,90,b,s,e -m c:\work\acuterig\msequenceterms
+..\..\bin\affflash -v -r 2 -l 6 -B 6 -o 1000 -t 4 -c 50 -d 1100 -b gray -m c:\work\acuterig\msequenceterms
+rem ..\..\bin\affflash -v -T -r 2 -l 10 -B 5 -t 50 -o 1000 -c 70 -d 1100 -b gray -g 1,1,2,2,78,1,0,90,b,s,e -m c:\work\acuterig\msequenceterms
