2a18e2b2616a7fa03b0bfcbab1e74fd6db36b657
devel <djsperka@ucdavis.edu>
2009-01-09 01:20:23 +0000

    Finally gave up on this. See AcuteFFFlash.

--- AlertRig/src/affflash/affflash.cpp
+++ AlertRig/src/affflash/affflash.cpp
@@ -40,6 +40,7 @@ ARGratingSpec f_grating1;
 bool f_bUseGrating = false;
 int f_nTermsOffset = 0;
 bool f_bNoSplit = false;
+bool f_bFlipTest = false;	// use 10101010.... instead of mseq. For testing only. 
 COLOR_TYPE f_colorBackground = { gray, {0.5, 0.5, 0.5}};
 COLOR_VECTOR_TYPE f_colorVector = { b_w, {0,0,0}, {1,1,1}};
 string f_sFilename;
@@ -54,10 +55,12 @@ int main (int argc, char *argv[])
 {
 	int nTerms = 0;
 	int nFramesBlank = 0;
+	int iterm;
 	double split_factor = 0.5;
 	VSGCYCLEPAGEENTRY cyclepage[32768];
 	int timeUS;
 	int ipage=0;
+	int i;
 
 	// Check input arguments
 	if (args(argc, argv))
@@ -65,12 +68,27 @@ int main (int argc, char *argv[])
 		return 1;
 	}
 
+	// INit vsg
+	if (ARvsg::instance().init(f_screenDistanceMM, f_colorBackground))
+	{
+		cerr << "VSG init failed!" << endl;
+		return 1;
+	}
+
+	// Compute basic parameters for the stimulus. 
+	split_factor = 0.5;
+	if (f_bNoSplit) split_factor = 1.0;
+	nTerms = split_factor * f_lengthSeconds * 1.0e6 / vsgGetSystemAttribute(vsgFRAMETIME) / f_iFramesPerTerm;
+	nFramesBlank = f_blankSeconds * 1.0e6 / vsgGetSystemAttribute(vsgFRAMETIME);
+
+
 	if (f_verbose)
 	{
 		cout << "affflash input parameters:" << endl;
-		cout << "Sequence length (s): " << f_lengthSeconds << endl;
+		if (f_bFlipTest) cout << "WARNING: MSEQ IGNORED. USING 101010101......!!!" << endl;
 		cout << "Split/repeat sequence for balance: " << (f_bNoSplit ? "NO" : "YES") << endl;
-		cout << "Blank length (s): " << f_blankSeconds << endl;
+		cout << "seq length(s): " << f_lengthSeconds << " frames_per_term: " << f_iFramesPerTerm << " nTerms: " << nTerms << endl;
+		cout << "blank length(s): " << f_blankSeconds << " blank terms : " << nFramesBlank << endl;
 		cout << "Offset terms: " << f_nTermsOffset <<endl;
 		cout << "Number of repeats: " << f_nRepeats << endl;
 		cout << "Contrast: " << f_contrast << " Color vector: " << f_colorVector << endl;
@@ -81,31 +99,19 @@ int main (int argc, char *argv[])
 			cout << "Grating specs: " << f_grating1 << endl;
 			cout << "Screen distance (mm): " << f_screenDistanceMM << endl;
 		}
+		if (f_bFlipTest) cout << "WARNING: MSEQ IGNORED. USING 101010101......!!!" << endl;
 	}
 
 
-	// INit vsg
-	if (ARvsg::instance().init(f_screenDistanceMM, f_colorBackground))
-	{
-		cerr << "VSG init failed!" << endl;
-		return 1;
-	}
 
-	// Basic parameters for the stimulus. 
-	split_factor = 0.5;
-	if (f_bNoSplit) split_factor = 1.0;
-	nTerms = split_factor * f_lengthSeconds * 1.0e6 / vsgGetSystemAttribute(vsgFRAMETIME) / f_iFramesPerTerm;
-	nFramesBlank = f_blankSeconds * 1.0e6 / vsgGetSystemAttribute(vsgFRAMETIME);
+	// Prepare pages. vsgVIDEOPAGE 0 (MSEQ_OFF_PAGE) and 1 (MSEQ_ON_PAGE) will represent 
+	// "off" and "on" terms from the msequence. 
+	// vsgOVERLAYPAGE 0 (OVERLAY_APERTURE_PAGE) will be used in conjunction with msequence
+	// terms - it is drawn with overlay level 0, which is "clear", in such a way that the 
+	// stimulus is seen when displayed. vsgOVERLAYPAGE 1 (OVERLAY_BLANK_PAGE) is drawn with
+	// the background color so it displays the background and blocks anything on the video 
+	// page. 
 
-	if (f_verbose)
-	{
-		cout << "seq length(s): " << f_lengthSeconds << " frames_per_term: " << f_iFramesPerTerm << " nTerms: " << nTerms << endl;
-		cout << "blank length(s): " << f_blankSeconds << " blank terms : " << nFramesBlank << endl;
-	}
-
-
-	// Prepare pages. Page 0 and 1 will be "off" and "on" terms from the msequence. 
-	// Page 2 will be the background (for the "blank" period). 
 	if (!f_bUseGrating)
 	{
 		COLOR_TYPE cton, ctoff;
@@ -138,22 +144,20 @@ int main (int argc, char *argv[])
 		f_grating0.init(75);
 		f_grating1.init(75);
 		
-		arutil_color_to_overlay_palette(f_colorBackground, 1);
 		vsgSetCommand(vsgOVERLAYMASKMODE);		// makes overlay pages visible
+		arutil_color_to_overlay_palette(f_colorBackground, 1);
 		vsgSetDrawPage(vsgOVERLAYPAGE, OVERLAY_APERTURE_PAGE, 1);
-		arutil_draw_aperture(f_grating0, 0);
+		arutil_draw_aperture(f_grating0, OVERLAY_APERTURE_PAGE);
 		vsgSetDrawPage(vsgOVERLAYPAGE, OVERLAY_BLANK_PAGE, 1);
+		vsgSetZoneDisplayPage(vsgOVERLAYPAGE, OVERLAY_BLANK_PAGE);
 
 		arutil_draw_grating(f_grating0, MSEQ_OFF_PAGE);
 		vsgPresent();
 		arutil_draw_grating(f_grating1, MSEQ_ON_PAGE);
 		vsgPresent();
-		vsgSetZoneDisplayPage(vsgOVERLAYPAGE, OVERLAY_BLANK_PAGE);
-//		vsgSetCommand(vsgDISABLELUTANIM);
-//		vsgSetCommand(vsgPALETTERAMP);
+		vsgSetCommand(vsgDISABLELUTANIM);
 
 	
-#if 0	
 	
 		{
 			int iquit=0;
@@ -165,15 +169,15 @@ int main (int argc, char *argv[])
 				switch(ipage)
 				{
 				case 0:
-					vsgSetZoneDisplayPage(vsgOVERLAYPAGE, 0);
-					vsgSetZoneDisplayPage(vsgVIDEOPAGE, 0);
+					vsgSetZoneDisplayPage(vsgOVERLAYPAGE, OVERLAY_APERTURE_PAGE);
+					vsgSetZoneDisplayPage(vsgVIDEOPAGE, MSEQ_OFF_PAGE);
 					break;
 				case 1:
-					vsgSetZoneDisplayPage(vsgOVERLAYPAGE, 0);
-					vsgSetZoneDisplayPage(vsgVIDEOPAGE, 1);
+					vsgSetZoneDisplayPage(vsgOVERLAYPAGE, OVERLAY_APERTURE_PAGE);
+					vsgSetZoneDisplayPage(vsgVIDEOPAGE, MSEQ_ON_PAGE);
 					break;
 				case 2:
-					vsgSetZoneDisplayPage(vsgOVERLAYPAGE, 1);
+					vsgSetZoneDisplayPage(vsgOVERLAYPAGE, OVERLAY_BLANK_PAGE);
 //					vsgSetZoneDisplayPage(vsgVIDEOPAGE, 2);
 					break;
 				}
@@ -182,49 +186,64 @@ int main (int argc, char *argv[])
 			}
 		}
 
-#endif
 	
 	}
 
 	// Set up page cycling
-	for (int i=0; i<nTerms; i++)
-	{
-		ipage = (f_mseq[i]=='1' ? MSEQ_ON_PAGE : MSEQ_OFF_PAGE);
-		cyclepage[i].Xpos = 0;
-		cyclepage[i].Ypos = 0;
-		cyclepage[i].Frames = f_iFramesPerTerm;
-		cyclepage[i].Stop=0;
-		cyclepage[i].Page = ipage + vsgTRIGGERPAGE;
-		cyclepage[i].ovPage = OVERLAY_APERTURE_PAGE;
-	}
 	if (!f_bNoSplit)
 	{
-		for (int i=0; i<nTerms; i++)
+		for (i=0; i<nTerms; i++)
 		{
-			ipage = (f_mseq[i]=='1' ? MSEQ_OFF_PAGE : MSEQ_ON_PAGE);
+			iterm = i + f_nTermsOffset;
+			if (f_bFlipTest) ipage = ((iterm % 2) ? MSEQ_ON_PAGE : MSEQ_OFF_PAGE);
+			else ipage = (f_mseq[iterm]=='1' ? MSEQ_ON_PAGE : MSEQ_OFF_PAGE);
+			cyclepage[i].Xpos = 0;
+			cyclepage[i].Ypos = 0;
+			cyclepage[i].Frames = f_iFramesPerTerm;
+			cyclepage[i].Stop=0;
+			cyclepage[i].Page = ipage + vsgDUALPAGE + vsgTRIGGERPAGE;
+			cyclepage[i].ovPage = OVERLAY_APERTURE_PAGE;
+		}
+		for (i=0; i<nTerms; i++)
+		{
+			iterm = i + f_nTermsOffset;
+			if (f_bFlipTest) ipage = ((iterm % 2) ? MSEQ_OFF_PAGE : MSEQ_ON_PAGE);
+			else ipage = (f_mseq[iterm]=='1' ? MSEQ_OFF_PAGE : MSEQ_ON_PAGE);
 			cyclepage[i+nTerms].Xpos = 0;
 			cyclepage[i+nTerms].Ypos = 0;
 			cyclepage[i+nTerms].Frames = f_iFramesPerTerm;
 			cyclepage[i+nTerms].Stop=0;
-			cyclepage[i+nTerms].Page = ipage + vsgTRIGGERPAGE;
+			cyclepage[i+nTerms].Page = ipage + vsgDUALPAGE + vsgTRIGGERPAGE;
 			cyclepage[i+nTerms].ovPage = OVERLAY_APERTURE_PAGE;
 		}
 		cyclepage[2*nTerms].Xpos = 0;
 		cyclepage[2*nTerms].Ypos = 0;
 		cyclepage[2*nTerms].Frames = nFramesBlank;
 		cyclepage[2*nTerms].Stop=0;
-		cyclepage[2*nTerms].Page = vsgTRIGGERPAGE;	// WHICH PAGE DOESN'T MATTER HERE. Overlay covers it. 
+		cyclepage[2*nTerms].Page = vsgDUALPAGE + vsgTRIGGERPAGE;	// WHICH PAGE DOESN'T MATTER HERE. Overlay covers it. 
 		cyclepage[2*nTerms].ovPage = OVERLAY_BLANK_PAGE;
 
 		vsgPageCyclingSetup(2*nTerms+1, &cyclepage[0]);
 	}
 	else
 	{
+		for (i=0; i<nTerms; i++)
+		{
+			iterm = i + f_nTermsOffset;
+			if (f_bFlipTest) ipage = ((iterm % 2) ? MSEQ_ON_PAGE : MSEQ_OFF_PAGE);
+			else ipage = (f_mseq[iterm]=='1' ? MSEQ_ON_PAGE : MSEQ_OFF_PAGE);
+			cyclepage[i].Xpos = 0;
+			cyclepage[i].Ypos = 0;
+			cyclepage[i].Frames = f_iFramesPerTerm;
+			cyclepage[i].Stop=0;
+			cyclepage[i].Page = ipage + vsgTRIGGERPAGE;
+			cyclepage[i].ovPage = OVERLAY_APERTURE_PAGE;
+		}
 		cyclepage[nTerms].Xpos = 0;
 		cyclepage[nTerms].Ypos = 0;
 		cyclepage[nTerms].Frames = nFramesBlank;
 		cyclepage[nTerms].Stop=0;
-		cyclepage[nTerms].Page = vsgTRIGGERPAGE;;
+		cyclepage[nTerms].Page = MSEQ_ON_PAGE + vsgTRIGGERPAGE;;
 		cyclepage[nTerms].ovPage = OVERLAY_BLANK_PAGE;
 		vsgPageCyclingSetup(nTerms+1, &cyclepage[0]);
 	}
@@ -240,7 +259,6 @@ int main (int argc, char *argv[])
 		cout << "Total stimulus length (us): " << timeUS << endl;
 	}
 	vsgResetTimer();
-	vsgSetCommand(vsgOVERLAYMASKMODE);		// makes overlay pages visible
 	vsgSetCommand(vsgCYCLEPAGEENABLE);
 	while (vsgGetTimer() < timeUS)
 	{
@@ -249,7 +267,9 @@ int main (int argc, char *argv[])
 	vsgSetCommand(vsgCYCLEPAGEDISABLE);
 	vsgSetZoneDisplayPage(vsgOVERLAYPAGE, OVERLAY_BLANK_PAGE);
 
-	if (f_mseq) free(&f_mseq);
+	cout << "Freeing mem" << endl;
+//	if (f_mseq) free(&f_mseq);
+	cout << "Freeing mem - done" << endl;
 
 	return 0;
 }
@@ -308,7 +328,7 @@ int args(int argc, char **argv)
 	bool have_t = false;
 	bool have_m = false;
 
-	while ((c = getopt(argc, argv, "r:B:l:o:ng:d:b:V:c:t:m:v")) != -1)
+	while ((c = getopt(argc, argv, "r:B:l:o:ng:d:b:V:c:t:m:Tv")) != -1)
 	{
 		switch (c) 
 		{
@@ -380,6 +400,9 @@ int args(int argc, char **argv)
 		case 'm':
 			f_sFilename.assign(optarg);
 			have_m = true;
+			break;
+		case 'T':
+			f_bFlipTest = true;
 			break;
 		case 'v':
 			f_verbose = true;
