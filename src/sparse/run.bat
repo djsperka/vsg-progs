0a2d1359fc86078efd8750eea21f76f75051da7b
devel <djsperka@ucdavis.edu>
2010-03-16 23:25:01 +0000

    New project added for sparse noise stimulus.

--- src/sparse/run.bat
+++ src/sparse/run.bat
@@ -1,1 +1,2 @@
-..\..\bin\sparse -d 8 -p -336,236 -r 16 -c 16 -t 50 -v
+rem ..\..\bin\sparse -d 8 -p -336,236 -r 16 -c 16 -t 50 -v -f stim/d27.snf
+..\..\bin\sparse -d 16 -p 0,0 -r 16 -c 16 -t 5 -v -f stim/d27.snf
