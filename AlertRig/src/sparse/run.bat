64df52c6fb2829d56a15060f2dba7b09e341c218
devel <djsperka@ucdavis.edu>
2010-03-19 00:03:52 +0000

    Add check for terms that are not visible on screen. This occurs if we allow large dot sizes. We can do that for sparse noise, but not msequence. When a dot will not be visible we make a blank term for it (all gray screen).

--- AlertRig/src/sparse/run.bat
+++ AlertRig/src/sparse/run.bat
@@ -1,2 +1,3 @@
 rem ..\..\bin\sparse -d 8 -p -336,236 -r 16 -c 16 -t 50 -v -f stim/d27.snf
-..\..\bin\sparse -d 28 -p 0,0 -r 16 -c 16 -t 5 -v -a -f stim/d27.snf
+rem ..\..\bin\sparse -d 28 -p 0,0 -r 16 -c 16 -t 5 -v -a -f stim/d27.snf
+..\..\bin\sparse -d 100 -p 0,0 -r 16 -c 16 -t 5 -v -a -f stim/d27.snf
