f7879587280a9cd3dbe12776d1cc30bc89c879bb
devel <djsperka@ucdavis.edu>
2011-10-12 19:37:31 +0000

    Command line args now must contain all target positions (as fixpt specs). Store those in a vector of ARFixationPointSpecs, not VSGOBJHANDLEs, and turn all of them off each time X trig received (this last thing was a hack to fix a bug in spike2 script). Changed m_ prefixes on globals to f_.

--- AlertRig/src/StarStim/StarStim.cpp
+++ AlertRig/src/StarStim/StarStim.cpp
@@ -21,27 +21,21 @@ using namespace alert;
 const double PI(4.0 * atan2(1.0, 1.0)); 
 
 // globals
-bool m_binaryTriggers = true;
-bool m_verbose = false;
-COLOR_TYPE m_background = { gray, {0, 0, 0}};
-int m_screenDistanceMM = 0;
-ARContrastFixationPointSpec m_fp;
-ARContrastFixationPointSpec m_target;
-vector<int> m_vecTargetOrder;
-vector<VSGOBJHANDLE> m_vecHandles;
-int m_nTargets=0;
-int m_iCurrentTarget = 0;
-TriggerVector m_triggers;
-double m_dOffsetDegrees = 0;
+bool f_binaryTriggers = true;
+bool f_verbose = false;
+COLOR_TYPE f_background = { gray, {0, 0, 0}};
+int f_screenDistanceMM = 0;
+ARContrastFixationPointSpec f_fp;
+vector<ARContrastFixationPointSpec*> f_targets;
+vector<int> f_vecTargetOrder;
+vector<int>::const_iterator f_iterator;
+TriggerVector f_triggers;
 
 // parse args
 int args(int argc, char **argv);
 
 // errors? dump usage and exit. 
 void usage();
-
-// Parse input target order file
-int parse_input_file(string filename, vector<int>& vec);
 
 void init_pages();
 void init_triggers();
@@ -55,7 +49,7 @@ int main(int argc, char **argv)
 	if (args(argc, argv)) return 1;
 
 	// Init vsg
-	if (ARvsg::instance().init(m_screenDistanceMM, m_background))
+	if (ARvsg::instance().init(f_screenDistanceMM, f_background))
 	{
 		cerr << "VSG init failed!" << endl;
 		return 1;
@@ -67,13 +61,13 @@ int main(int argc, char **argv)
 
 	// Issue "ready" triggers to spike2.
 	// These commands pulse spike2 port 6. 
-	vsgObjSetTriggers(vsgTRIG_ONPRESENT + vsgTRIG_OUTPUTMARKER, 0x20, 0);
-	vsgPresent();
-	vsgObjSetTriggers(vsgTRIG_ONPRESENT + vsgTRIG_OUTPUTMARKER, 0x00, 0);
-	vsgPresent();
 
-	m_triggers.reset(vsgIOReadDigitalIn());
+	ARvsg::instance().ready_pulse();
 
+	f_triggers.reset(vsgIOReadDigitalIn());
+
+	// Initialize the target order iterator
+	f_iterator = f_vecTargetOrder.begin();
 
 	// All right, start monitoring triggers........
 	string s;
@@ -81,15 +75,15 @@ int main(int argc, char **argv)
 	while (1)
 	{
 		// If user-triggered, get a trigger entry. 
-		if (!m_binaryTriggers)
+		if (!f_binaryTriggers)
 		{
 			// Get a new "trigger" from user
 			cout << "Enter trigger/key: ";
 			cin >> s;
 		}
 
-		TriggerFunc	tf = std::for_each(m_triggers.begin(), m_triggers.end(), 
-			(m_binaryTriggers ? TriggerFunc(vsgIOReadDigitalIn(), last_output_trigger) : TriggerFunc(s, last_output_trigger)));
+		TriggerFunc	tf = std::for_each(f_triggers.begin(), f_triggers.end(), 
+			(f_binaryTriggers ? TriggerFunc(vsgIOReadDigitalIn(), last_output_trigger) : TriggerFunc(s, last_output_trigger)));
 
 		// Now analyze input trigger
 	 	
@@ -108,76 +102,48 @@ int main(int argc, char **argv)
 }
 
 
-// Generate target objects and draw them. 
-// Assumes m_target has been parsed, m_vecHandles exists, and m_nTargets has been parsed as well. 
-// Creates 'm_nTargets' fix points. Create the one for m_target, then rotate them around the center point. 
-
-void generate_targets()
-{
-	double angle;
-	double offset;
-	double xorigin, yorigin;
-
-	angle = 2*PI/m_nTargets;
-	offset = m_dOffsetDegrees * PI/180;
-	xorigin = m_fp.x;
-	yorigin = m_fp.y;
-
-#if 0
-  	m_target.init(2);
-	m_target.setContrast(0);
-	m_target.draw();
-	m_vecHandles.push_back(m_target.handle());
-#endif
-
-	// Now take m_target.x,y and rotate. 
-	for (int i=0; i<m_nTargets; i++)
-	{
-		ARContrastFixationPointSpec t;
-		double theta = angle * i + offset;
-		t.x = (m_target.x - xorigin) * cos(theta) - (m_target.y - yorigin) * sin(theta) + xorigin;
-		t.y = (m_target.x - xorigin) * sin(theta) + (m_target.y - yorigin) * cos(theta) + yorigin;
-		t.d = m_target.d;
-		t.color = m_target.color;
-		t.init(2);
-		t.setContrast(0);
-		t.draw();
-		m_vecHandles.push_back(t.handle());
-	}
-}
 
 void init_pages()
 {
 
 	// initialize video pages
-	if (ARvsg::instance().init(m_screenDistanceMM, m_background))
+	if (ARvsg::instance().init(f_screenDistanceMM, f_background))
 	{
 		cerr << "VSG video initialization failed!" << endl;
 	}
 
 	vsgSetDrawPage(vsgVIDEOPAGE, 0, vsgBACKGROUND);
 
-	m_fp.init(2);
-	m_fp.setContrast(0);
-	m_fp.draw();
+	f_fp.init(2);
+	f_fp.setContrast(0);
+	f_fp.draw();
 
-	generate_targets();
+	for (unsigned int i=0; i<f_targets.size(); i++)
+	{
+		if (f_verbose) 
+		{
+			cout << "Target " << i << " : " << *f_targets[i] << endl;
+		}
+		f_targets[i]->init(2);
+		f_targets[i]->setContrast(0);
+		f_targets[i]->draw();
+	}
 	
 	vsgPresent();
-
 }
 
 
 void init_triggers()
 {
 	// triggers for stim will be CallbackTriggers, all using the same callback function
-	m_triggers.addTrigger(new CallbackTrigger("F", 0x2, 0x2, 0x2, 0x2, callback));
-	m_triggers.addTrigger(new CallbackTrigger("f", 0x2, 0x0, 0x2, 0x0, callback));
-	m_triggers.addTrigger(new CallbackTrigger("S", 0x4, 0x4, 0x4, 0x4, callback));
-	m_triggers.addTrigger(new CallbackTrigger("s", 0x4, 0x0, 0x4, 0x0, callback));
-	m_triggers.addTrigger(new CallbackTrigger("a", 0x8, 0x8 | AR_TRIGGER_TOGGLE, 0x8, 0x8 | AR_TRIGGER_TOGGLE, callback));
-	m_triggers.addTrigger(new QuitTrigger("q", 0x10, 0x10, 0xff, 0x0, 0));
+	f_triggers.addTrigger(new CallbackTrigger("F", 0x2, 0x2, 0x2, 0x2, callback));
+	f_triggers.addTrigger(new CallbackTrigger("f", 0x2, 0x0, 0x2, 0x0, callback));
+	f_triggers.addTrigger(new CallbackTrigger("S", 0x4, 0x4, 0x4, 0x4, callback));
+	f_triggers.addTrigger(new CallbackTrigger("s", 0x4, 0x0, 0x4, 0x0, callback));
+	f_triggers.addTrigger(new CallbackTrigger("a", 0x8, 0x8 | AR_TRIGGER_TOGGLE, 0x8, 0x8 | AR_TRIGGER_TOGGLE, callback));
+	f_triggers.addTrigger(new QuitTrigger("q", 0x10, 0x10, 0xff, 0x0, 0));
 }
+
 
 
 
@@ -192,109 +158,90 @@ int callback(int &output, const CallbackTrigger* ptrig)
 	string key = ptrig->getKey();
 	if (key == "a")
 	{
-		m_iCurrentTarget++;
-		if (m_iCurrentTarget == m_vecTargetOrder.size()) m_iCurrentTarget = 0;
+		f_iterator++;
+		if (f_iterator == f_vecTargetOrder.end()) f_iterator = f_vecTargetOrder.begin();
 	}
 	else if (key == "s")
 	{
-		vsgObjSelect(m_vecHandles[m_vecTargetOrder[m_iCurrentTarget]]);
-		vsgObjSetContrast(0);
+		f_targets[*f_iterator]->setContrast(0);
+		for (vector<int>::iterator iter = f_vecTargetOrder.begin(); iter < f_vecTargetOrder.end(); iter++)
+		{
+			f_targets[*iter]->setContrast(0);
+		}
 	}
 	else if (key == "S")
 	{
-		vsgObjSelect(m_vecHandles[m_vecTargetOrder[m_iCurrentTarget]]);
-		vsgObjSetContrast(100);
+		f_targets[*f_iterator]->setContrast(100);
 	}
 	else if (key == "F")
 	{
-		m_fp.setContrast(100);
+		f_fp.setContrast(100);
 	}
 	else if (key == "f")
 	{
-		m_fp.setContrast(0);
+		f_fp.setContrast(0);
 	}
 
 	return ival;
 }
 
-
-
-// Open the file 'filename' and read ints into the vector. Return 0 if the file is found and read OK, 
-// 1 otherwise (writes err msg to stderr). 
-
-int parse_input_file(string filename, vector<int>& vec)
-{
-	// Now open parfile and write that number into it, then close it. 
-	ifstream in;
-	int i;
-	in.open(filename.c_str(), std::ios::in);
-	if (!in)
-	{
-		cerr << "Cannot open input file " << filename;
-		return 1;
-	}
-
-	while (in >> i) vec.push_back(i);
-	in.close();
-
-	return 0;
-}
-
-
 int args(int argc, char **argv)
 {	
 	bool have_f=false;		// have fixation spec
-	bool have_t=false;		// have target spec
 	bool have_d=false;		// have screen dist
-	bool have_n=false;		// have # of targets
-	bool have_r=false;		// set if randomized order file is provided (not required)
 	string s;
 	int c;
 	extern char *optarg;
 	extern int optind;
 	int errflg = 0;
-	while ((c = getopt(argc, argv, "avf:t:b:d:hn:r:o:")) != -1)
+	ARContrastFixationPointSpec *pspec;
+
+	while ((c = getopt(argc, argv, "avf:t:b:d:ho:")) != -1)
 	{
 		switch (c) 
 		{
 		case 'a':
-			m_binaryTriggers = false;
+			f_binaryTriggers = false;
 			break;
 		case 'v':
-			m_verbose = true;
+			f_verbose = true;
 			break;
 		case 'f': 
 			s.assign(optarg);
-			if (parse_fixation_point(s, m_fp)) errflg++;
+			if (parse_fixation_point(s, f_fp)) errflg++;
 			else have_f = true;
 			break;
 		case 't': 
 			s.assign(optarg);
-			if (parse_fixation_point(s, m_target)) errflg++;
-			else have_t = true;
+			pspec = new ARContrastFixationPointSpec();
+			s.assign(optarg);
+			if (!parse_fixation_point(s, *pspec))
+			{
+				f_targets.push_back(pspec);
+			}
+			else 
+			{
+				errflg++;
+				cerr << "Error in target spec (" << s << ")" << endl;
+				delete(pspec);
+			}
 			break;
 		case 'b': 
 			s.assign(optarg);
-			if (parse_color(s, m_background)) errflg++; 
+			if (parse_color(s, f_background)) errflg++; 
 			break;
 		case 'd':
 			s.assign(optarg);
-			if (parse_distance(s, m_screenDistanceMM)) errflg++;
+			if (parse_distance(s, f_screenDistanceMM)) errflg++;
 			else have_d=true;
-			break;
-		case 'n':
-			s.assign(optarg);
-			if (parse_integer(s, m_nTargets)) errflg++;
-			else have_n=true;
-			break;
-		case 'r':
-			s.assign(optarg);
-			if (parse_input_file(s, m_vecTargetOrder)) errflg++;
-			else have_r = true;
 			break;
 		case 'o':
 			s.assign(optarg);
-			if (parse_double(s, m_dOffsetDegrees)) errflg++;
+			if (parse_int_list(s, f_vecTargetOrder))
+			{
+				cerr << "Error in target order list. Expecting comma-separated list of integers." << endl;
+				errflg++;
+			}
 			break;
 		case 'h':
 			errflg++;
@@ -313,27 +260,26 @@ int args(int argc, char **argv)
 		cerr << "Fixation point not specified!" << endl; 
 		errflg++;
 	}
-	if (!have_t) 
-	{
-		cerr << "Target point not specified!" << endl; 
-		errflg++;
-	}
 	if (!have_d)
 	{
 		cerr << "Screen distance not specified!" << endl; 
 		errflg++;
 	}
-	if (!have_n)
+
+	// Make sure there were some targets
+	if (f_targets.size()==0)
 	{
-		cerr << "Number of targets not specified!" << endl; 
+		cerr << "No targets specified!" << endl;
 		errflg++;
 	}
 
-	// If no random order file specified, populate the target order vector with 1,2,...,n
-	if (!errflg && !have_r)
+	// If no random order specified, populate the target order vector with 1,2,...,n
+	if (!errflg && f_vecTargetOrder.size()==0)
 	{
-		for (int i=0; i < m_nTargets; i++) m_vecTargetOrder.push_back(i);
+		for (unsigned int i=0; i < f_targets.size(); i++) f_vecTargetOrder.push_back(i);
 	}
+
+
 
 	if (errflg) 
 	{
@@ -345,6 +291,6 @@ int args(int argc, char **argv)
 
 void usage()
 {
-	cerr << "usage: starstim -f x,y,d[,color] -d screen_distance_MM -b g|b|w -s x,y,w,h,contrast%,sf,tf,orientation,color_vector,s|q,r|e" << endl;
+	cerr << "usage: starstim -f x,y,d[,color] -d screen_distance_MM -b g|b|w -t x,y,d,[color] [-t x,y,d,[color] ...] [-o n0,n1,n2,...]" << endl;
 }
 
--- AlertRig/src/StarStim/starstim.bat
+++ AlertRig/src/StarStim/starstim.bat
@@ -2,7 +2,9 @@ rem d:\work\alertrig\bin\starstim -a -b gray -f 0,0,1,r -t 5,0,1,r -n 5 -d 555
 rem test offset, second stim should be at 90 deg (directly above fixpt)
 rem d:\work\alertrig\bin\starstim -a -b gray -f 0,0,1,r -t 5,0,1,r -n 5 -d 555 -o 18
 rem same as offset test above, but fixpt is at 3,3, not 0,0.
-d:\work\alertrig\bin\starstim -a -b gray -f 3,3,1,r -t 8,3,1,r -n 5 -d 555 -o 18
+rem d:\work\alertrig\bin\starstim -a -b gray -f 3,3,1,r -t 8,3,1,r -n 5 -d 555 -o 18
 rem c:\work\alertrig\bin\starstim -b gray -f 0,0,1,r -t 5,0,1,r -n 5 -d 555
 rem c:\work\alertrig\bin\starstim -b gray -f 0,0,1,r -t 4,4,1,r -n 7 -d 555
 rem c:\work\alertrig\bin\starstim -b gray -f 0,0,1,r -t 4,0,1,r -n 5 -d 555 -r starstim.txt
+rem c:\sysinternals\psexec \\192.168.1.1 -c ..\..\bin\starstim -d 500 -b gray -a -v -f 0,0,.5,red -t 5,0,.5,blue -t 5,1,.5,blue -t 5,2,.5,blue -t 5,3,.5,blue -t 5,4,.5,blue -t 5,5,.5,blue 
+c:\sysinternals\psexec \\192.168.1.1 -c ..\..\bin\starstim -d 500 -b gray -a -v -f 0,0,.5,red -t 5,0,.5,blue -t 5,1,.5,blue -t 5,2,.5,blue -t 5,3,.5,blue -t 5,4,.5,blue -t 5,5,.5,blue -o 5,4,3,2,1,0
