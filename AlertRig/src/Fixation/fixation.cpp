d4dc2367f3b01c554aab5c36e5a2db4a66d4ed42
devel <djsperka@ucdavis.edu>
2013-03-27 22:50:35 +0000

    Modify this application to work with calibration and regular fixation. In calibration, there is a 3x3 grid of fixpt positions, where 1,1 is lower left, and 3,3 is upper right. Offset value given by the calibration offset on command line (this is required for running in calibration mode). Triggers will cause target point to be redrawn at the requested location, and shoudl be called when fixpt is off (otherwise the newly drawn fixpt will be shown immediately).

--- AlertRig/src/Fixation/fixation.cpp
+++ AlertRig/src/Fixation/fixation.cpp
@@ -18,22 +18,26 @@
 
 #pragma comment (lib, "vsgv8.lib")
 
-int args(int argc, char **argv);
-void init_triggers();
-
 using namespace std;
 using namespace alert;
 
-ARContrastFixationPointSpec m_afp;
-COLOR_TYPE m_background;
-vector<ARGratingSpec*> m_distractors;
-int m_screenDistanceMM=0;
-bool m_verbose=false;
-TriggerVector triggers;
-bool m_binaryTriggers = true;
-bool m_bCalibration = false;
-double m_dCalibrationOffset = 0.0;
+int args(int argc, char **argv);
+void init_triggers();
+int callback(int &output, const CallbackTrigger* ptrig);
+
+ARContrastFixationPointSpec f_afp;
+COLOR_TYPE f_background;
+vector<ARGratingSpec*> f_distractors;
+int f_screenDistanceMM=0;
+bool f_verbose=false;
+TriggerVector f_triggers;
+bool f_binaryTriggers = true;
+bool f_bCalibration = false;
+double f_dCalibrationOffset = 0.0;
 int f_pulse = 0x2;
+int f_iCount = 0;
+double f_dFixationX = 0;
+double f_dFixationY = 0;
 
 static void usage();
 static int init_pages();
@@ -48,21 +52,21 @@ int main (int argc, char *argv[])
 	}
 	else
 	{
-		if (m_verbose)
+		if (f_verbose)
 		{
-			cout << "Screen distance " << m_screenDistanceMM << endl;
-			cout << "Fixation point " << m_afp << endl;
-			cout << "Background color " << m_background << endl;
-			for (unsigned int i=0; i<m_distractors.size(); i++)
+			cout << "Screen distance " << f_screenDistanceMM << endl;
+			cout << "Fixation point " << f_afp << endl;
+			cout << "Background color " << f_background << endl;
+			for (unsigned int i=0; i<f_distractors.size(); i++)
 			{
-				cout << "Distractor " << (i+1) << ": " << m_distractors[i] << endl;
+				cout << "Distractor " << (i+1) << ": " << f_distractors[i] << endl;
 			}
 		}
 	}
 
 
 	// INit vsg
-	if (ARvsg::instance().init(m_screenDistanceMM, m_background))
+	if (ARvsg::instance().init(f_screenDistanceMM, f_background))
 	{
 		cerr << "VSG init failed!" << endl;
 		return 1;
@@ -91,19 +95,19 @@ int main (int argc, char *argv[])
 	// Start monitoring triggers........
 	std::string s;
 	int last_output_trigger=0;
-	triggers.reset(vsgIOReadDigitalIn());
+	f_triggers.reset(vsgIOReadDigitalIn());
 	while (1)
 	{
 		// If user-triggered, get a trigger entry. 
-		if (!m_binaryTriggers)
+		if (!f_binaryTriggers)
 		{
 			// Get a new "trigger" from user
 			cout << "Enter trigger/key: ";
 			cin >> s;
 		}
 
-		TriggerFunc	tf = std::for_each(triggers.begin(), triggers.end(), 
-			(m_binaryTriggers ? TriggerFunc(vsgIOReadDigitalIn(), last_output_trigger) : TriggerFunc(s, last_output_trigger)));
+		TriggerFunc	tf = std::for_each(f_triggers.begin(), f_triggers.end(), 
+			(f_binaryTriggers ? TriggerFunc(vsgIOReadDigitalIn(), last_output_trigger) : TriggerFunc(s, last_output_trigger)));
 
 		// Now analyze input trigger
 	 	
@@ -145,23 +149,28 @@ int args(int argc, char **argv)
 		switch (c) 
 		{
 		case 'a':
-			m_binaryTriggers = false;
+			f_binaryTriggers = false;
 			break;
 		case 'v':
-			m_verbose = true;
+			f_verbose = true;
 			break;
 		case 'f': 
 			s.assign(optarg);
-			if (parse_fixation_point(s, m_afp)) errflg++;
-			else have_f = true;
+			if (parse_fixation_point(s, f_afp)) errflg++;
+			else 
+			{
+				have_f = true;
+				f_dFixationX = f_afp.x;
+				f_dFixationY = f_afp.y;
+			}
 			break;
 		case 'b': 
 			s.assign(optarg);
-			if (parse_color(s, m_background)) errflg++; 
+			if (parse_color(s, f_background)) errflg++; 
 			break;
 		case 'd':
 			s.assign(optarg);
-			if (parse_distance(s, m_screenDistanceMM)) errflg++;
+			if (parse_distance(s, f_screenDistanceMM)) errflg++;
 			else have_d=true;
 			break;
 		case 'g':
@@ -169,7 +178,7 @@ int args(int argc, char **argv)
 			s.assign(optarg);
 			if (!parse_grating(s, *pspec))
 			{
-				m_distractors.push_back(pspec);
+				f_distractors.push_back(pspec);
 			}
 			else errflg++;
 			break;
@@ -178,14 +187,14 @@ int args(int argc, char **argv)
 			break;
 		case 'C':
 			s.assign(optarg);
-			if (parse_double(s, m_dCalibrationOffset))
+			if (parse_double(s, f_dCalibrationOffset))
 			{
 				cerr << "Cannot parse calibration offset!" << endl;
 				errflg++;
 			}
 			else
 			{
-				m_bCalibration = true;
+				f_bCalibration = true;
 			}
 			break;
 		case '?':
@@ -222,19 +231,114 @@ void usage()
 
 void init_triggers()
 {
-	triggers.addTrigger(new PageTrigger("0", 0x2, 0x0, 0xff, 0x0, 0));
-	triggers.addTrigger(new PageTrigger("1", 0x2, 0x2, 0xff, 0x2, 1));
-	triggers.addTrigger(new QuitTrigger("q", 0x40, 0x40, 0xff, 0x0, 0));
+	std::vector<std::pair<std::string, int> >vecInputs;
+
+	f_triggers.addTrigger(new PageTrigger("F", 0x2, 0x2, 0x2, 0x2, 1));
+	//f_triggers.addTrigger(new CallbackTrigger("S", 0x4, 0x4, 0x4, 0x4, callback));
+	//f_triggers.addTrigger(new CallbackTrigger("s", 0x4, 0x0, 0x4, 0x0, callback));
+	f_triggers.addTrigger(new PageTrigger("X", 0x2, 0x0, 0x2, 0x0, 0));
+	//f_triggers.addTrigger(new CallbackTrigger("a", 0x8, 0x8|AR_TRIGGER_TOGGLE, 0x8, 0x8|AR_TRIGGER_TOGGLE, callback));
+
+	// MultiInput, single output trigger
 
 
-		// Dump triggers
+	vecInputs.push_back(std::pair< string, int>("3", 0x1c));
+	vecInputs.push_back(std::pair< string, int>("2", 0x18));
+	vecInputs.push_back(std::pair< string, int>("1", 0x14));
+	vecInputs.push_back(std::pair< string, int>("6", 0x2c));
+	vecInputs.push_back(std::pair< string, int>("5", 0x28));
+	vecInputs.push_back(std::pair< string, int>("4", 0x24));
+	vecInputs.push_back(std::pair< string, int>("9", 0x3c));
+	vecInputs.push_back(std::pair< string, int>("8", 0x38));
+	vecInputs.push_back(std::pair< string, int>("7", 0x34));
+	f_triggers.push_back(new MultiInputSingleOutputCallbackTrigger(vecInputs, 0x3c, 0x8, 0x8|AR_TRIGGER_TOGGLE, callback));
+
+	// quit trigger
+	f_triggers.addTrigger(new QuitTrigger("q", 0x40, 0x40, 0xff, 0x0, 0));
+
+#if 0
+
+	f_triggers.addTrigger(new PageTrigger("0", 0x2, 0x0, 0xff, 0x0, 0));
+	f_triggers.addTrigger(new PageTrigger("1", 0x2, 0x2, 0xff, 0x2, 1));
+	f_triggers.addTrigger(new CallbackTrigger("a", 0x4, 0x4, 0x4, 0x4, callback));
+
+	f_triggers.addTrigger(new QuitTrigger("q", 0x40, 0x40, 0xff, 0x0, 0));
+
+#endif
+
+	// Dump triggers
 	std::cout << "Triggers:" << std::endl;
-	for (unsigned int i=0; i<triggers.size(); i++)
+	for (unsigned int i=0; i<f_triggers.size(); i++)
 	{
-		std::cout << "Trigger " << i << " " << *(triggers[i]) << std::endl;
+		std::cout << "Trigger " << i << " " << *(f_triggers[i]) << std::endl;
 	}
 
 
+}
+
+
+// The return value from this trigger callback determines whether a vsgPresent() is issued. 
+
+int callback(int &output, const CallbackTrigger* ptrig)
+{
+	int ival=1;
+	string key = ptrig->getKey();
+
+	if (f_bCalibration)
+	{
+		int ix = 0, iy = 0;
+		if (key == "1")
+		{
+			ix = 1; 
+			iy = 1;
+		}
+		else if (key == "2")
+		{
+			ix = 2;
+			iy = 1;
+		}
+		else if (key == "3")
+		{
+			ix = 3;
+			iy = 1;
+		}
+		else if (key == "4")
+		{
+			ix = 1; 
+			iy = 2;
+		}
+		else if (key == "5")
+		{
+			ix = 2;
+			iy = 2;
+		}
+		else if (key == "6")
+		{
+			ix = 3;
+			iy = 2;
+		}
+		else if (key == "7")
+		{
+			ix = 1; 
+			iy = 3;
+		}
+		else if (key == "8")
+		{
+			ix = 2;
+			iy = 3;
+		}
+		else if (key == "9")
+		{
+			ix = 3;
+			iy = 3;
+		}
+		f_afp.x = f_dCalibrationOffset * (ix-2);
+		f_afp.y = f_dCalibrationOffset * (iy-2);
+		vsgSetDrawPage(vsgVIDEOPAGE, STIMULUS_PAGE, vsgBACKGROUND);
+		f_afp.draw();
+		ival = 0;	// do not give vsgPresent()
+	}
+	return ival;
 }
 
 int init_pages()
@@ -242,21 +346,21 @@ int init_pages()
 	int status=0;
 	vsgSetDrawPage(vsgVIDEOPAGE, STIMULUS_PAGE, vsgBACKGROUND);
 
-	if (m_distractors.size() > 0)
+	if (f_distractors.size() > 0 && !f_bCalibration)
 	{
 		// determine the level slice for each
-		int islice = 250/m_distractors.size();
+		int islice = 250/f_distractors.size();
 		if (islice > 50) islice=50;
-		for (unsigned int i=0; i<m_distractors.size(); i++)
+		for (unsigned int i=0; i<f_distractors.size(); i++)
 		{
-			m_distractors[i]->init(islice);
-			m_distractors[i]->draw();
+			f_distractors[i]->init(islice);
+			f_distractors[i]->draw();
 		}
 	}
 
 	// draw fixpt last so it isn't overwritten by stimuli
-	m_afp.init(2);
-	m_afp.draw();
+	f_afp.init(2);
+	f_afp.draw();
 
 	vsgSetDrawPage(vsgVIDEOPAGE, BACKGROUND_PAGE, vsgBACKGROUND);
 	vsgPresent();
