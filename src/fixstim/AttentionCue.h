306f2fb9dd6638e48e40853f60a4ffe3c56943fe
djsperka <djsperka@ucdavis.edu>
2018-06-29 17:15:30 +0000

    Broke code for parsing and managing attention cues out of AttentionStimSet so I can use it elsewhere.

---
+++ src/fixstim/AttentionCue.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <Alertlib.h>
+
+// Struct for specifying cues (dots/rect) in attention
+typedef struct attention_cue
+{
+	double rdiff;
+	int linewidth;
+	COLOR_TYPE color;
+} AttentionCue;
+
+int parse_attcues(const std::string& s, int nstim, std::vector<AttentionCue>& vecCues);
+void dump_attcues(const std::vector<AttentionCue>& vecCues);
--- src/fixstim/AttentionStimSet.cpp
+++ src/fixstim/AttentionStimSet.cpp
@@ -18,55 +18,6 @@ double get_fconfig(VSGCYCLEPAGEENTRY* cycle, int count)
 
 
 
-int parse_attcues(const string& s, int nstim, vector<AttentionCue>& vecCues)
-{
-	COLOR_TYPE color;
-	double rdiff;
-	int linewidth;
-	unsigned int i;
-	vector<string> tokens;
-	istringstream iss;
-	AttentionCue acue;
-	tokenize(s, tokens, ",");
-	if (tokens.size() % (nstim*3) == 0)
-	{
-		for (i=0; i<tokens.size()/3; i++)
-		{
-
-			// expect rdiff,linewidth,color
-			// rdiff is difference between radius of circle and corresponding grating
-			// linewidth is width of circle outline, in pixels (must be integer)
-
-			if (parse_double(tokens[i*3], rdiff) || parse_integer(tokens[i*3+1], linewidth) || parse_color(tokens[i*3+2], color))
-			{
-				cerr << "Error reading attention cues (" << s << ")" << endl;
-				return 1;
-			}
-			acue.rdiff = rdiff;
-			acue.linewidth = linewidth;
-			acue.color = color;
-			vecCues.push_back(acue);
-
-		}
-	}
-	else
-	{
-		cerr << "Error reading attention cues (" << s << ") Expecting a multiple of " << nstim*3 << " args, 3 for each stim." << endl;
-		return 1;
-	}
-	return 0;
-}
-
-
-void my_dump_attcues(const AttentionCue& acue)
-{
-	cerr << "Q: rdiff=" << acue.rdiff << ", linewidth=" << acue.linewidth << ", color=" << acue.color << endl;
-}
-
-void dump_attcues(const vector<AttentionCue>& vecCues)
-{
-	for_each(vecCues.begin(), vecCues.end(), my_dump_attcues);
-}
 
 
 
@@ -306,10 +257,6 @@ int checkFlashyTimes(const vector<AttParams>& vecInput, const FlashyParamVectorV
 	}
 	return status;
 }
-
-
-
-
 
 int parse_interleaved_params(const string& s, int nstim, vector<InterleavedParams>& params)
 {
