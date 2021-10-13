e69c00db99ecf761ce729e11aa37cadb42ee12b8
Daniel Sperka <djsperka@ucdavis.edu>
2021-10-13 11:08:21 -0700

    Copy submodule header files

--- AlertRig/src/example1/tcpserver.cpp
+++ AlertRig/src/example1/tcpserver.cpp
@@ -50,25 +50,11 @@ public:
             boost::bind(&tcp_connection::handle_read, shared_from_this(), 
                 boost::asio::placeholders::error,
                 boost::asio::placeholders::bytes_transferred));
-#if 0
-        message_ = make_daytime_string();
-
-        boost::asio::async_write(socket_, boost::asio::buffer(message_),
-            boost::bind(&tcp_connection::handle_write, shared_from_this(),
-                boost::asio::placeholders::error,
-                boost::asio::placeholders::bytes_transferred));
-#endif
-
     }
 
 private:
     tcp_connection(boost::asio::io_context& io_context)
         : socket_(io_context)
-    {
-    }
-
-    void handle_write(const boost::system::error_code& /*error*/,
-        size_t /*bytes_transferred*/)
     {
     }
 
