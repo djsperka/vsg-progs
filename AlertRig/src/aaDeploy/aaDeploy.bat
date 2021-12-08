628a4051ca2cda33a3f216dc16c19cdf25794990
Daniel Sperka <djsperka@ucdavis.edu>
2021-12-08 12:34:06 -0800

    Reorganize build dirs for lib&bin. Make projects use it, and aaDeploy will also create zip files correctly.

--- AlertRig/src/aaDeploy/aaDeploy.bat
+++ AlertRig/src/aaDeploy/aaDeploy.bat
@@ -1,12 +1,10 @@
 rem 
-rem
-rem for %%f in ("%1:,=" "%") do copy /f /y %2\%%f %3\%%f
-rem for %%f in (%~1) do copy /y %~2\%%f %~3\%%f
-rem
-rem new stuff here
 rem call with command line in Custom Build Step
 rem
-rem call aaDeploy.bat "dir where bin and demo are"  "full path to output zip file"
+rem call aaDeploy.bat "dir where bin is"  "relative path to exe files" "dir for output zip"
+rem 
+rem e.g.
+rem call aaDeploy.bat "E:\work\vsg-progs\AlertRig" x64\Release e:\work\vsg-progs\AlertRig
 rem 
 rem I am only copying three exe files and demo/. To add another to deployment zip file
 rem just change the 7z command line.
@@ -18,6 +16,11 @@ pushd %~dp0
 rem get output of git describe
 for /f %%i in ('git describe') do set VER=%%i
 cd %1
-7z a %2\alertrig-%VER%.zip bin\fixstim.exe bin\CableTest.exe bin\remote.exe demo
-
+copy bin\%2\fixstim.exe bin 
+copy bin\%2\remote.exe bin 
+copy bin\%2\CableTest.exe bin
+7z a %3\alertrig-%VER%.zip bin\fixstim.exe bin\CableTest.exe bin\remote.exe
+del bin\fixstim.exe 
+del bin\remote.exe 
+del bin\CableTest.exe
 popd
