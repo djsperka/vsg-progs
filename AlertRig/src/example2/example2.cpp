cef4363ebb21d5cbbbb952719212baa68497e2fb
Daniel Sperka <djsperka@ucdavis.edu>
2024-03-27 13:07:20 -0700

    Fixes to Debug build config. Also add/remove files from examples.

--- AlertRig/src/example2/example2.cpp
+++ AlertRig/src/example2/example2.cpp
@@ -21,8 +21,8 @@ using namespace alert;
 
 int main (int argc, char *argv[])
 {
-	string s;
-	COLOR_TYPE background = { gray, {0.5, 0.5, 0.5}};
+	string s, s2;
+	COLOR_TYPE background = gray;
 	int dist = 555;
 
 	ARvsg::instance().init(dist, background);
@@ -32,37 +32,39 @@ int main (int argc, char *argv[])
 	vsgUnit2Unit(vsgPIXELUNIT,dScrWidth,vsgDEGREEUNIT,&dScrWidth);
 	vsgUnit2Unit(vsgPIXELUNIT,dScrHeight,vsgDEGREEUNIT,&dScrHeight);
 
-	// Initialize and draw overlay page
-	if (ARvsg::instance().init_overlay())
-	{
-		cerr << "VSG overlay initialization failed!" << endl;
-		return 1;
-	}
 
-	// Set overlay page to all clear (level 0), then set level 1 to red, and draw a dot. 
-	vsgSetDrawPage(vsgOVERLAYPAGE, 0, 0);
-
-
-	VSGTRIVAL red;
-	red.a=1; red.b = red.c = 0;
-	vsgPaletteWriteOverlayCols((VSGLUTBUFFER*)&red, 1, 1);
-	vsgSetPen1(1);
-	vsgDrawOval(0, 0, 1, 1);
-
-	vsgSetPen1(0);
-	vsgDrawOval(2,3,4,4);
-
-
-	vsgSetDrawPage(vsgVIDEOPAGE, 0, vsgBACKGROUND);
-	vsgSetDisplayPage(0);
-
+	ARContrastFixationPointSpec fixpt;
 	ARGratingSpec gr;
 	ostringstream oss;
-	oss << "0,0," << dScrWidth << "," << dScrHeight << ",100,3,3,30,b,s,e";
+	oss << "0,0,100,100,100,3,3,30,b,s,e";
 	if (parse_grating(oss.str(), gr))
 	{
 		cerr << "Error parsing gr string: " << oss.str() << endl;
 	}
+	oss << "0,0,1,green";
+	if (parse_fixation_point("0,0,1,green", fixpt))
+	{
+		cerr << "Error parsing fixpt string" << endl;
+	}
+	fixpt.init(2, false);
+
+
+	vsgSetCommand(vsgOVERLAYMASKMODE);
+
+	// Set overlay page to all clear (level 0), then set level 1 to red, and draw a dot. 
+	vsgSetDrawPage(vsgOVERLAYPAGE, 0, 0);
+	fixpt.drawOverlay(1);
+
+	//VSGTRIVAL red;
+	//red.a=1; red.b = red.c = 0;
+	//vsgPaletteWriteOverlayCols((VSGLUTBUFFER*)&red, 1, 1);
+	//vsgSetPen1(1);
+	//vsgDrawOval(0, 0, 1, 1);
+
+	vsgSetDrawPage(vsgVIDEOPAGE, 0, vsgBACKGROUND);
+	vsgSetDisplayPage(0);
+
+
 
 	// try draw page 1
 	vsgSetDrawPage(vsgVIDEOPAGE, 1, vsgBACKGROUND);
@@ -74,48 +76,37 @@ int main (int argc, char *argv[])
 	vsgPresent();
 	vsgSetDisplayPage(1);
 
-	cout << "Enter move coords: "; 
+	cout << "Enter m(move fixpt),q,d,o: "; 
 	cin >> s;
 	while (s != "q" && s!= "Q")
 	{
-
-
-		if (s == "m" || s=="M")
+		if (s == "m")
 		{
-			while (1)
+			cout << "Enter fixpt spec: ";
+			cin >> s2;
+			if (parse_fixation_point(s2, fixpt))
 			{
-				double ax, ay;
-				double mx, my;
-				POINT pos;
-				GetCursorPos(&pos);
-				ax = pos.x / dScrWidth;
-				ay = pos.y / dScrHeight;
-//				mx = floor(1024 - ax*800);
-//				my = floor(600 - ay*600);
-				mx = floor(400 - ax*800);
-				my = floor(300 - ay*600);
-				if (mx < 0) mx = 0;
-				if (my < 0) my = 0;
-				cout << "xm=" << mx << " ym=" << my << endl;
-				if (vsgMoveScreen(mx, my) < 0)
-				{
-					cerr << "Command failed" <<endl;
-				}
+				cerr << "Error parsing fixpt string, try again: ";
+			}
+			else
+			{
+				cout << "Moving fixpt to " << fixpt << endl;
+				vsgSetCommand(vsgOVERLAYMASKMODE);
+				vsgSetDrawPage(vsgOVERLAYPAGE, 0, 0);
+				fixpt.drawOverlay(1);
 			}
 		}
-		else
+		else if (s == "d")
 		{
-			double x, y;
-			if (!parse_xy(s, x, y))
-			{
-				cout << "Moving " << x << " " << y << endl;
-				if (vsgMoveScreen(x, y) < 0)
-				{
-					cerr << "Command failed" <<endl;
-				}
-			}
+			cout << "vsgSetCommand(vsgOVERLAYDISABLE);" << endl;
+			vsgSetCommand(vsgOVERLAYDISABLE);
 		}
-		cout << "Enter move dist or q: ";
+		else if (s == "o")
+		{
+			cout << "vsgSetCommand(vsgOVERLAYMASKMODE);" << endl;
+			vsgSetCommand(vsgOVERLAYMASKMODE);
+		}
+		cout << "Enter m(move fixpt),q,d,o: ";
 		cin >> s;
 	}
 
